950096b148492b75c8421336e7a406d65e00e509
MatheusJose-tech <matheusjoseofficial243@gmail.com>
2026-09-20 09:36:22 -0300

    refactor: cria constante de mapeamento de produtos

---
+++ estoque/constante.py
@@ -0,0 +1,7 @@
+from armazem import *
+
+PRODUTOS = {
+    "Aged Brie": Queijo_envelhecido,
+    "Sulfuras, Hand of Ragnaros": Sulfuras,
+    "Backstage passes to a TAFKAL80ETC concert": Ingressos_de_show
+}
