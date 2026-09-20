16721d621dca9bb9eaa7f8239e47c4779772c11d
Tonidev <antoniodev2709@gmail.com>
2026-09-20 10:25:28 -0300

    refactor: refatora adição de produtos na loja

---
+++ loja/loja.py
@@ -0,0 +1,23 @@
+from estoque.armazem import Normal
+from estoque.constante import PRODUTOS
+
+class Loja:  
+    def __init__(self, produtos):
+        self.produtos = []
+
+
+    def adicionar_produtos(self, produtos):
+        for produto in produtos:
+            classe_produto = PRODUTOS.get(produto.nome, Normal)
+
+            novo_produto = classe_produto(
+                produto.nome,
+                produto.dias_vendas,
+                produto.qualidade
+            )
+
+            self.produtos.append(novo_produto)
+
+    def atualizar_produtos(self):
+        for produto in self.produtos:
+            produto.atualizar()
